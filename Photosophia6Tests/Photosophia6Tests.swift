fd32cf82cf5a83742227e9b0dcf5140d98d8f3fc
nor <nor>
2019-03-31 22:21:50 +0700

    Initial Commit

---
+++ Photosophia6Tests/Photosophia6Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Photosophia6Tests.swift
+//  Photosophia6Tests
+//
+//  Created by norsez on 31/3/19.
+//  Copyright © 2019 Bluedot. All rights reserved.
+//
+
+import XCTest
+@testable import Photosophia6
+
+class Photosophia6Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
