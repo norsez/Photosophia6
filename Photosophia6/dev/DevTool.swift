bfcff31f1aa46f7225b1cb04389afbc94e21b764
nor <nor>
2019-04-25 19:28:37 +0700

    add activity item

---
+++ Photosophia6/dev/DevTool.swift
@@ -0,0 +1,9 @@
+//
+//  DevTool.swift
+//  Photosophia6
+//
+//  Created by norsez on 25/4/19.
+//  Copyright © 2019 Bluedot. All rights reserved.
+//
+
+import Foundation
--- Photosophia6/view controllers/InterestingPhotosViewController+PhotoBrowser.swift
+++ Photosophia6/view controllers/InterestingPhotosViewController+PhotoBrowser.swift
@@ -9,17 +9,36 @@
 import UIKit
 import SKPhotoBrowser
 
+class PhotoActivityProvider: UIActivityItemProvider {
+    let photo: Photo
+    init(with photo: Photo) {
+        self.photo = photo
+        super.init(placeholderItem: photo)
+    }
+    
+    override var item: Any {
+        get {
+            return self.photo.photoWebURL as Any
+        }
+    }
+}
+
 extension InterestingPhotosViewController {
     func showPhoto(from indexPath: IndexPath) {
-        let skPhotos = self.viewModel.photos.value.compactMap { (p) -> SKPhoto? in
-            let sk_photo = SKPhoto.photoWithImageURL(p.url_c ?? "")
-            sk_photo.shouldCachePhotoURLImage = true
-            let caption = self.viewModel.caption(of: p)
-            sk_photo.caption = caption
-            return sk_photo
-        }
+//        let skPhotos = self.viewModel.photos.value.compactMap { (p) -> SKPhoto? in
+//            let sk_photo = SKPhoto.photoWithImageURL(p.url_c ?? "")
+//            sk_photo.shouldCachePhotoURLImage = true
+//            let caption = self.viewModel.caption(of: p)
+//            sk_photo.caption = caption
+//            return sk_photo
+//        }
+        let photo = self.viewModel.photos.value[indexPath.item]
+        let skPhoto = SKPhoto.photoWithImageURL(photo.url_c ?? "")
+        skPhoto.shouldCachePhotoURLImage = true
+        skPhoto.caption = self.viewModel.caption(of: photo)
+        let browser = SKPhotoBrowser(photos: [skPhoto], initialPageIndex: indexPath.row)
+        browser.activityItemProvider = PhotoActivityProvider(with: photo)
         
-        let browser = SKPhotoBrowser(photos: skPhotos, initialPageIndex: indexPath.row)
         self.present(browser, animated: true, completion: nil)
     }
 }
